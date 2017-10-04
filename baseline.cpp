06e139aa1d811e9e3432b40027a2bb68a5c9539c
fai516 <ysryb@mst.edu>
2017-10-03 23:40:49 -0500

    update

--- baseline.cpp
+++ baseline.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <cerrno>
+#include <map>
 #include <unistd.h>
 #include <string>
 #include "mpi.h"
@@ -9,7 +10,18 @@
 
 using namespace std;
 
-int main ( int argc, char *argv[] ) 
+std::string decodeMsg(int n){
+  std::string out;
+  switch(n){
+  case 0:out = "requesting to eat";break;
+  case 1:out = "stopping to eat(wanting to think)";break;
+  case 2:out = "allowed to eat";break;
+  default:out ="waiting";break;
+  }
+  return out;
+}
+
+int main ( int argc, char *argv[] )
 {
   int id; //my MPI ID
   int p;  //total MPI processes
@@ -24,7 +36,7 @@ int main ( int argc, char *argv[] )
 
   //  Determine the rank of this process.
   id = MPI::COMM_WORLD.Get_rank ( );
-  
+
   //Safety check - need at least 2 philosophers to make sense
   if (p < 3) {
 	    MPI::Finalize ( );
@@ -34,51 +46,74 @@ int main ( int argc, char *argv[] )
 
   srand(id + time(NULL)); //ensure different seeds...
 
-//  cout << "This is process " << id << " of " << p 
+//  cout << "This is process " << id << " of " << p
 //       << " I'll give you a random number: " << rand() << endl;
-	
+
   //  Setup Fork Master (Ombudsman) and Philosophers
   if ( id == 0 ) //Master
   {
 	int msgIn; //messages are integers
-	cout << id << endl;  
 	//let the philosophers check in
+
+    /* Philosophers eating status */
+    map<int,bool> eatingStat;
+
+
+    //cout << forks[0]+1234 << endl;
     for (int i = 1; i < p; i++) {
-		MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, MPI::ANY_SOURCE, tag, status );
-		std::cout << "Receiving message " << msgIn << " from Philosopher ";
-//OLD SYNTAX		std::cout << status.getSource() << std::endl;
-		std::cout << status.Get_source() << std::endl;
-	}
+  		MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, MPI::ANY_SOURCE, tag, status );
+  		std::cout << "Receiving message " << decodeMsg(msgIn) << " from Philosopher ";
+      std::cout << status.Get_source() << std::endl;
+
+      /* Starting waiter method*/
+      int target = status.Get_source(); //get the # of philosopher
+      if(msgIn == 0){ //requesting to eat
+        int left = target+1; //his left side
+        int right = target-1; //his right side
+        /* handling edge case */
+        if(left > 5) left = 1; //if the left side is philosopher 6, that means it actually is philosopher 1
+        if(right < 1) right = 5; //same idea as above
+        if(eatingStat[left]==false || eatingStat[right]==false){ //adjesent not eating => forks is available
+          cout << "@@Philosopher " << target << " is allowed to eat!!" << endl;;
+          eatingStat[target]=true;
+        }
+        else{
+          cout << "@@Philosopher " << target << " can't eat because his neighbour is eating!!" << endl;
+        }
+      }
+      else if(msgIn == 1){
+        if(eatingStat[target]==true){
+          cout << "@@Philosopher " << target << " stopping to eat!";
+          eatingStat[target]=false;
+        }
+        else{
+          cout << "@@Philosopher " << target << " can't stop eating because he isn't even eating!";
+        }
+      }
+      else{
+        cout << "@@Waiter: You can do whatever you want but eat!" << endl;
+      }
+	  }
   }
   else //I'm a philosopher
   {
-	cout << id << endl;
-/* NOTE: The following code sends a random integer (message) back to the server. You do 
+	//cout << id << endl;
+/* NOTE: The following code sends a random integer (message) back to the server. You do
  *
- *                            * * * * * * * * NOT * * * * * * * * 
+ *                            * * * * * * * * NOT * * * * * * * *
  *
  * want to send a random message in the solution... you want a specific message (integer)
- * to mean something specific (like requesting to eat, being allowed to eat, returning 
+ * to mean something specific (like requesting to eat, being allowed to eat, returning
  * resources, etc.)
  */
     int msgOut = rand() % p; //pick a number between 0 and the number of philosophers
-	std::string msg;
-	switch(msgOut){
-	case 0:msg = "0msg";break;
-	case 1:msg = "requesting to eat";break;
-	case 2:msg = "being allowed to eat";break;
-	case 3:msg = "3msg";break;
-	case 4:msg = "4msg";break;
-	case 5:msg = "5msg";break;
-	default: cout << "Busted" << endl;break;
-	}
-	
-	std::cout << "This is Philosopher " << id << " sending message of " << msg << std::endl;
-	
+
+    msgOut = 0;
+	std::cout << "This is Philosopher " << id << " sending message of " << decodeMsg(msgOut) << std::endl;
+
    	//check in with master - send a random number (node 0 = master)
-	MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, 0, tag ); 
+	MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, 0, tag );
   }
-  
   //  Terminate MPI.
   MPI::Finalize ( );
   return 0;
--- makefile
+++ makefile
@@ -1,10 +1,12 @@
-EXECS=mpi_hello_world
-MPICC?=mpicc
+EXECS=mpi_phi_prob
+MPICXX?=mpicxx
+MPIRUN?=mpirun
 
 all: ${EXECS}
 
-mpi_hello_world: mpi_hello_world.c
-    ${MPICC} -o mpi_hello_world mpi_hello_world.c
+mpi_phi_prob: baseline.cpp
+	${MPICXX} -o run baseline.cpp
+	${MPIRUN} -n 6 run
 
 clean:
-    rm ${EXECS}
+	rm ${EXECS}
